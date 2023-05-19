d81605930f0e768572176431f6607aa57e8d4006
Peterskill <104547222+Peterskill@users.noreply.github.com>
2023-05-19 22:14:54 +0530

    new policy changes

--- terraformautomation/main.tf
+++ terraformautomation/main.tf
@@ -11,6 +11,19 @@ resource "aws_dynamodb_table" "dynamotablecreation" {
 }
 resource "aws_iam_role" "auto-role" {
   name = "aws-lambda-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
 
 }
 resource "aws_iam_role_policy_attachment" "auto-role-policy1" {
